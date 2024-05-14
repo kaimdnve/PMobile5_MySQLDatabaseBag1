ef6facfe507fd674fb7ec4677954ddaa8f4c4836
kaimdnve <kaimdnve@gmail.com>
2024-05-14 21:15:31 +0800

    a

--- Mahasiswa.php
+++ Mahasiswa.php
@@ -1,5 +1,9 @@
 <?php
 class Mahasiswa {
+    public $stb;
+    public $nama;
+    public $angkatan;
+
     function __construct($stb, $nama, $angkatan) {
         $this->stb = $stb;
         $this->nama = $nama;
