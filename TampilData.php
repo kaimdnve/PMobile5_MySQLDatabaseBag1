67b4810edab6ffdf66f2b9169a3c46b499582c83
kaimdnve <kaimdnve@gmail.com>
2024-05-15 13:00:17 +0800

    a

--- TampilData.php
+++ TampilData.php
@@ -1,13 +1,13 @@
 <?php
-include 'Koneksi.php';
-include 'Mahasiswa.php';
+    include 'Koneksi.php';
+    include 'Mahasiswa.php';
 
-$hasil = array();
-$result = $con->query("SELECT * FROM tb_mhs");
-$i = 0;
-while ($record = $result->fetch_assoc()) {
-    $hasil[$i] = new Mahasiswa($record["stb"], $record["nama"], $record["angkatan"]);
-    $i++;
-}
-echo json_encode($hasil);
+    $hasil = array();
+    $result = $con->query("SELECT * FROM tb_mhs");
+    $i = 0;
+    while ($record = $result->fetch_assoc()) {
+        $hasil[$i] = new Mahasiswa($record["stb"], $record["nama"], $record["angkatan"]);
+        $i++;
+    }
+    echo json_encode($hasil);
 ?>
